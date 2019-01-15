6ac06c1976dcdad3b3176ee50d2f170747b6ea7c
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-15 01:28:46 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+  new_array = []
+  collection.each do |item|
+    val = yield(item)
+    if (val == true)
+      new_array.push(item)
+    end
+  end
+  new_array
 end
