a6d970e4beaf13c4f93094cc9ade051c42330569
github username <github email address>
2019-01-20 13:33:57 -0800

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
+# nums = [1, 2, 3, 4, 5]
+
 def my_select(collection)
- # your code here!
+  i = 0
+  answer = []
+
+  while i < collection.length
+	if yield(collection[i]) == true
+	  answer << collection[i]
+	end
+
+	i += 1
+  end
+
+  answer
 end
+
+# my_select(nums) do |x|
+#     x.even?
+# end
