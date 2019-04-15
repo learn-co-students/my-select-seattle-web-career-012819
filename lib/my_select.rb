428dbc1062140332e01df24320abed664cc64450
Amy Cohen <amy.cohen@flatironschool.com>
2019-04-15 16:00:03 -0700

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
  # your code here!
+
+    if collection.length == 0
+        return collection
+    end
+
+    count = 0
+    mod_collection = []
+    while count < collection.length
+        if yield(collection[count])
+            mod_collection << collection[count]
+        end
+        count += 1
+    end
+
+    mod_collection
+
 end
